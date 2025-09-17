0bba7fdcae3f54674e6b5198c379d0bb63506b64
ankitneogi <ankitneogiwork@gmail.com>
2025-09-17 18:08:23 +0700

    Update VideoSection.tsx

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -4,10 +4,24 @@ import styles from "./VideoSection.module.css";
 export default function VideoSection() {
   return (
     <section className={styles.videoSection}>
-      <div className={styles.textContainer}>
-        <h1>
-          The only program in which you earn by editing your content with CapCut, without new dedicated content needed.
-        </h1>
+      <div className={styles.container}>
+        {/* Left Column: Text */}
+        <div className={styles.textContainer}>
+          <h1>
+            Hear From HOME's Leadership
+          </h1>
+        </div>
+
+        {/* Right Column: Video */}
+        <div className={styles.videoContainer}>
+          <iframe
+            width="100%"
+            height="400"
+            src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </section>
   );
